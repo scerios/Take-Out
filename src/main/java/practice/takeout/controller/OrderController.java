975549f017d84e257b565e619635178e9bc17229
Takács Róbert <89.t.robert@gmail.com>
2019-06-21 11:46:11 +0200

    feat(OrderController): added getOrderPage()

--- src/main/java/practice/takeout/controller/OrderController.java
+++ src/main/java/practice/takeout/controller/OrderController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import practice.takeout.model.Meal;
@@ -39,5 +40,15 @@ public class OrderController {
       meal.setType("all");
     }
     return "redirect:/order/{id}";
+  }
+
+  @GetMapping("/order{id}")
+  public String getOrderPage(@PathVariable long id, Model model) {
+    if (mealService.getMealById(id) == null) {
+      return "error";
+    } else {
+      model.addAttribute("meal", mealService.getMealById(id));
+      return "order";
+    }
   }
 }
