52705533448d0d7a5c5f6585868ea4551666a298
Takács Róbert <89.t.robert@gmail.com>
2019-06-21 10:30:06 +0200

    feat(OrderController): instanciated service + added constructor

--- src/main/java/practice/takeout/controller/OrderController.java
+++ src/main/java/practice/takeout/controller/OrderController.java
@@ -1,7 +1,15 @@
 package practice.takeout.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import practice.takeout.service.MealServiceImpl;
 
 @Controller
 public class OrderController {
+  private MealServiceImpl mealService;
+
+  @Autowired
+  public OrderController(MealServiceImpl mealService) {
+    this.mealService = mealService;
+  }
 }
