7c447431554abee955e0178c7cb24e5d72a343f8
Takács Róbert <89.t.robert@gmail.com>
2019-06-21 16:26:08 +0200

    feat(OrderStatUpdDto): added Dto to patch status through api

---
+++ src/main/java/practice/takeout/dto/OrderStatUpdDto.java
@@ -0,0 +1,13 @@
+package practice.takeout.dto;
+
+public class OrderStatUpdDto {
+  private String status;
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+}
