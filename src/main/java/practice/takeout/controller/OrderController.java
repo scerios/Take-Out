d5553acb2f081f3f529109bcfab9cb3815d197f3
Takács Róbert <89.t.robert@gmail.com>
2019-06-21 11:24:48 +0200

    fix(OrderController): added getPages()

--- src/main/java/practice/takeout/controller/OrderController.java
+++ src/main/java/practice/takeout/controller/OrderController.java
@@ -2,7 +2,10 @@ package practice.takeout.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import practice.takeout.model.Meal;
 import practice.takeout.service.MealServiceImpl;
 
 @Controller
@@ -12,6 +15,11 @@ public class OrderController {
   @Autowired
   public OrderController(MealServiceImpl mealService) {
     this.mealService = mealService;
+  }
+
+  @ModelAttribute
+  public void getPages(Model model) {
+    model.addAttribute("meal", new Meal());
   }
 
   @GetMapping("/")
