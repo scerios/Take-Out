653321523be885581877e01e410c806213a023fc
Takács Róbert <89.t.robert@gmail.com>
2019-06-21 17:34:20 +0200

    feat(service + impl): implemented iterables + dto

--- src/main/java/practice/takeout/service/MealService.java
+++ src/main/java/practice/takeout/service/MealService.java
@@ -1,9 +1,13 @@
 package practice.takeout.service;
 
+import practice.takeout.dto.MealDetailsDto;
 import practice.takeout.model.Meal;
 
 public interface MealService {
   void addMeal(Meal meal);
   Meal getMealById(long id);
   Meal saveMeal(Meal meal);
+  MealDetailsDto MealToDto(Meal meal);
+  Iterable<Meal> getAllMealsByStatus(String status);
+  Iterable<Meal> getAllMealsByTypeAndStatus(String type, String status);
 }
--- src/main/java/practice/takeout/service/MealServiceImpl.java
+++ src/main/java/practice/takeout/service/MealServiceImpl.java
@@ -1,6 +1,7 @@
 package practice.takeout.service;
 
 import org.springframework.stereotype.Service;
+import practice.takeout.dto.MealDetailsDto;
 import practice.takeout.model.Meal;
 import practice.takeout.repository.MealRepository;
 
@@ -26,5 +27,24 @@ public class MealServiceImpl implements MealService {
   public Meal saveMeal(Meal meal) {
     this.repository.save(meal);
     return meal;
+  }
+
+  @Override
+  public MealDetailsDto MealToDto(Meal meal) {
+    MealDetailsDto mealDetailsDto = new MealDetailsDto();
+    mealDetailsDto.setId(meal.getId());
+    mealDetailsDto.setName(meal.getName());
+    mealDetailsDto.setBase(meal.getBase());
+    mealDetailsDto.setTopping(meal.getTopping());
+    mealDetailsDto.setStatus(meal.getStatus());
+    return mealDetailsDto;
+  }
+
+  public Iterable<Meal> getAllMealsByStatus(String status) {
+    return repository.findAllByStatus(status);
+  }
+
+  public Iterable<Meal> getAllMealsByTypeAndStatus(String type, String status) {
+    return repository.findAllByTypeAndStatus(type, status);
   }
 }
