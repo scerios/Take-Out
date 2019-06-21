6d4cdee0202b63b26f800b43e02cf82fcffb4a2e
Takács Róbert <89.t.robert@gmail.com>
2019-06-21 11:36:49 +0200

    feat(OrderController): added sendOrder()

--- src/main/java/practice/takeout/controller/OrderController.java
+++ src/main/java/practice/takeout/controller/OrderController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import practice.takeout.model.Meal;
 import practice.takeout.service.MealServiceImpl;
 
@@ -25,5 +27,17 @@ public class OrderController {
   @GetMapping("/")
   public String getIndex() {
     return "index";
+  }
+
+  @PostMapping("/order")
+  public String sendOrder(Meal meal, RedirectAttributes redirectAttributes) {
+    mealService.addMeal(meal);
+    redirectAttributes.addAttribute("id", meal.getId());
+    if (meal.getTopping().equals("Smoked Tofu")) {
+      meal.setType("vegetarian");
+    } else {
+      meal.setType("all");
+    }
+    return "redirect:/order/{id}";
   }
 }
