68d8390986dc9fbb7ca4401ddb89634647e1c346
Takács Róbert <89.t.robert@gmail.com>
2019-06-21 10:27:56 +0200

    feat(MealService): instanciated repository + added constructor

--- src/main/java/practice/takeout/service/MealServiceImpl.java
+++ src/main/java/practice/takeout/service/MealServiceImpl.java
@@ -1,7 +1,13 @@
 package practice.takeout.service;
 
 import org.springframework.stereotype.Service;
+import practice.takeout.repository.MealRepository;
 
 @Service
 public class MealServiceImpl implements MealService {
+  private MealRepository repository;
+
+  public MealServiceImpl(MealRepository repository) {
+    this.repository = repository;
+  }
 }
