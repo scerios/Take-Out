7345cac6de6e404fb7eecf9fdab91800939158a5
Takács Róbert <89.t.robert@gmail.com>
2019-06-21 10:31:57 +0200

    feat(OrderController): added getIndex()

--- src/main/java/practice/takeout/controller/OrderController.java
+++ src/main/java/practice/takeout/controller/OrderController.java
@@ -2,6 +2,7 @@ package practice.takeout.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import practice.takeout.service.MealServiceImpl;
 
 @Controller
@@ -11,5 +12,10 @@ public class OrderController {
   @Autowired
   public OrderController(MealServiceImpl mealService) {
     this.mealService = mealService;
+  }
+
+  @GetMapping
+  public String getIndex() {
+    return "index";
   }
 }
