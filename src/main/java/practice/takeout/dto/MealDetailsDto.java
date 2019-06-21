31e95478ebd94e4873054df2a4ee3accbe993cf1
Takács Róbert <89.t.robert@gmail.com>
2019-06-21 11:58:56 +0200

    feat(MealDetailsDto): added DTO for meals

---
+++ src/main/java/practice/takeout/dto/MealDetailsDto.java
@@ -0,0 +1,58 @@
+package practice.takeout.dto;
+
+public class MealDetailsDto {
+  private long id;
+  private String name;
+  private String address;
+  private String base;
+  private String topping;
+  private String status;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getBase() {
+    return base;
+  }
+
+  public void setBase(String base) {
+    this.base = base;
+  }
+
+  public String getTopping() {
+    return topping;
+  }
+
+  public void setTopping(String topping) {
+    this.topping = topping;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+}
