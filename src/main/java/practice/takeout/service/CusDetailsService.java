3c4f5fb848c510ecc6e996872f0c26d176f3aee5
Takács Róbert <89.t.robert@gmail.com>
2019-06-23 14:23:24 +0200

    feat(CusDetails + Impl): added logic to prevent registering same email

--- src/main/java/practice/takeout/service/CusDetailsService.java
+++ src/main/java/practice/takeout/service/CusDetailsService.java
@@ -5,4 +5,5 @@ import practice.takeout.model.CusDetails;
 public interface CusDetailsService {
   void addDetails(CusDetails cusDetails);
   CusDetails getDetailsById(long id);
+  boolean isEmailAlreadyRegistered(String email);
 }
