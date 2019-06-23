516f52a70042371bf454de12846027a083184a9d
Takács Róbert <89.t.robert@gmail.com>
2019-06-23 14:21:53 +0200

    feat(SystemDefaults): added config.prop handler

---
+++ src/main/java/practice/takeout/dataProvider/SystemDefaults.java
@@ -0,0 +1,58 @@
+package practice.takeout.dataProvider;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class SystemDefaults {
+  private Properties properties;
+  private final String PROPERTY_FILE_PATH = "configs/Configuration.properties";
+  private final String NOT_SPECIFIED_ERR_MSG = " is not specified in the Configuration.properties file.";
+
+  public SystemDefaults() {
+    try (BufferedReader reader = new BufferedReader(new FileReader(PROPERTY_FILE_PATH))) {
+      properties = new Properties();
+      properties.load(reader);
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException("Cannot find specific file.");
+    } catch (IOException e) {
+      throw new RuntimeException("Cannot read/write specific file.");
+    } catch (Exception e) {
+      throw new RuntimeException("Cannot identify the problem.");
+    }
+  }
+
+  public String getJdbcDriver() {
+    String jdbcDriver = properties.getProperty("jdbcDriver");
+    if (jdbcDriver != null) {
+      return jdbcDriver;
+    }
+    throw new RuntimeException("JDBC Driver" + NOT_SPECIFIED_ERR_MSG);
+  }
+
+  public String getDbUrl() {
+    String dbUrl = properties.getProperty("dbUrl");
+    if (dbUrl != null) {
+      return dbUrl;
+    }
+    throw new RuntimeException("DB URL" + NOT_SPECIFIED_ERR_MSG);
+  }
+
+  public String getUserName() {
+    String userName = properties.getProperty("userName");
+    if (userName != null) {
+      return userName;
+    }
+    throw new RuntimeException("Username" + NOT_SPECIFIED_ERR_MSG);
+  }
+
+  public String getPwd() {
+    String pwd = properties.getProperty("pwd");
+    if (pwd != null) {
+      return pwd;
+    }
+    throw new RuntimeException("Password" + NOT_SPECIFIED_ERR_MSG);
+  }
+}
