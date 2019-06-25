d6ba87193d751a83a68001513d5805c9e16effcd
Takács Róbert <89.t.robert@gmail.com>
2019-06-25 21:03:24 +0200

    feat(Cus): added pin field as String for reference

--- src/main/java/practice/takeout/model/Cus.java
+++ src/main/java/practice/takeout/model/Cus.java
@@ -13,6 +13,7 @@ public class Cus {
   private long id;
   private String email;
   private String pwd;
+  private String pin;
 
   @OneToMany(mappedBy = "cus", cascade = CascadeType.ALL)
   private List<CusDetails> cusDetailsList;
@@ -42,6 +43,14 @@ public class Cus {
 
   public void setPwd(String pwd) {
     this.pwd = pwd;
+  }
+
+  public String getPin() {
+    return pin;
+  }
+
+  public void setPin(String pin) {
+    this.pin = pin;
   }
 
   public List<CusDetails> getCusDetailsList() {
