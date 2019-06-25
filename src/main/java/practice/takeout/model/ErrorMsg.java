c98157770a0d339b7e8c5918b3ead61886670899
Takács Róbert <89.t.robert@gmail.com>
2019-06-25 21:27:05 +0200

    feat(ErrorMsg): added new fields for new errors

--- src/main/java/practice/takeout/model/ErrorMsg.java
+++ src/main/java/practice/takeout/model/ErrorMsg.java
@@ -1,13 +1,31 @@
 package practice.takeout.model;
 
 public class ErrorMsg {
-  private String errorMsg;
+  private String alreadyRegistered;
+  private String wrongEmail;
+  private String wrongPwd;
 
-  public String getErrorMsg() {
-    return errorMsg;
+  public String getAlreadyRegistered() {
+    return alreadyRegistered;
   }
 
-  public void setErrorMsg(String errorMsg) {
-    this.errorMsg = errorMsg;
+  public void setAlreadyRegistered(String alreadyRegistered) {
+    this.alreadyRegistered = alreadyRegistered;
+  }
+
+  public String getWrongEmail() {
+    return wrongEmail;
+  }
+
+  public void setWrongEmail(String wrongEmail) {
+    this.wrongEmail = wrongEmail;
+  }
+
+  public String getWrongPwd() {
+    return wrongPwd;
+  }
+
+  public void setWrongPwd(String wrongPwd) {
+    this.wrongPwd = wrongPwd;
   }
 }
