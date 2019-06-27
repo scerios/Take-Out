6744369141659425a271e562a5a105c064644dc8
Takács Róbert <89.t.robert@gmail.com>
2019-06-27 17:18:58 +0200

    refactor(): @Get /homepage moved to IndexController

--- src/main/java/practice/takeout/controller/IndexController.java
+++ src/main/java/practice/takeout/controller/IndexController.java
@@ -1,7 +1,32 @@
 package practice.takeout.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import practice.takeout.model.ErrorMsg;
+import practice.takeout.service.CusServiceImpl;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
+  private CusServiceImpl cusService;
+
+  @Autowired
+  public IndexController(CusServiceImpl cusService) {
+    this.cusService = cusService;
+  }
+
+  @GetMapping("/homepage")
+  public String getHomePage(Model model, HttpSession session, ErrorMsg errorMsg, RedirectAttributes redirectAttributes) {
+    if (cusService.isCusHasAccess(session)) {
+      model.addAttribute("cus", cusService.getCusById(cusService.getCusSessionId(session)));
+      return "homepage";
+    } else {
+      cusService.accessDenied(errorMsg, redirectAttributes);
+      return "redirect:/";
+    }
+  }
 }
