5bf9676dd12e15e05132923ee3d3516cdcb48ac7
Takács Róbert <89.t.robert@gmail.com>
2019-07-11 16:58:08 +0200

    feat(OrderController): addBurgerToCart tested

--- src/main/java/practice/takeout/controller/OrderController.java
+++ src/main/java/practice/takeout/controller/OrderController.java
@@ -2,34 +2,40 @@ package practice.takeout.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import practice.takeout.model.Burger;
+import practice.takeout.model.OrderDetails;
 import practice.takeout.service.BurgerServiceImpl;
+import practice.takeout.service.CusServiceImpl;
+import practice.takeout.service.OrderDetailsServiceImpl;
+import practice.takeout.service.OrderServiceImpl;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class OrderController {
+  private OrderServiceImpl orderService;
+  private OrderDetailsServiceImpl orderDetailsService;
+  private CusServiceImpl cusService;
   private BurgerServiceImpl burgerService;
 
   @Autowired
-  public OrderController(BurgerServiceImpl burgerService) {
+  public OrderController(OrderServiceImpl orderService, OrderDetailsServiceImpl orderDetailsService,
+                         CusServiceImpl cusService, BurgerServiceImpl burgerService) {
+    this.orderService = orderService;
+    this.orderDetailsService = orderDetailsService;
+    this.cusService = cusService;
     this.burgerService = burgerService;
   }
 
-  @ModelAttribute
-  public void getPage(Model model) {
-    model.addAttribute("burger", new Burger());
-  }
-
   @PostMapping("/order")
-  public String addBurgerToCart(Burger burger) {
+  public String addBurgerToCart(Burger burger, OrderDetails orderDetails, HttpSession session) {
     long burgerId = burgerService.getBurgerIdByQuery(burger.getBun(), burger.getMeat(), burger.getCheese(), burger.getSauce());
     if (burgerId == 0) {
       burgerService.addBurger(burger);
-    } else {
-      System.out.println(burgerService.getBurgerById(burgerId).getBun());
+      burgerId = burgerService.getBurgerIdByQuery(burger.getBun(), burger.getMeat(), burger.getCheese(), burger.getSauce());
     }
+    orderDetailsService.addOrderDetails(orderDetails, burgerId);
+    orderService.addOrder(cusService.getCusSessionId(session));
     return "redirect:/order";
   }
 }
