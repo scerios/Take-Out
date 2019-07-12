e328240e3ffb94321e4bf9448edceb88cdd64beb
Takács Róbert <89.t.robert@gmail.com>
2019-07-12 12:10:43 +0200

    refactor(ProfileController): deleted unused code

--- src/main/java/practice/takeout/controller/ProfileController.java
+++ src/main/java/practice/takeout/controller/ProfileController.java
@@ -2,7 +2,6 @@ package practice.takeout.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import practice.takeout.model.CusDetails;
@@ -10,7 +9,6 @@ import practice.takeout.model.Cus;
 import practice.takeout.model.ErrorMsg;
 import practice.takeout.service.CusDetailsServiceImpl;
 import practice.takeout.service.CusServiceImpl;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
@@ -23,12 +21,6 @@ public class ProfileController {
   public ProfileController(CusServiceImpl cusService, CusDetailsServiceImpl cusDetailsService) {
     this.cusService = cusService;
     this.cusDetailsService = cusDetailsService;
-  }
-
-  @ModelAttribute
-  public void getPage(Model model) {
-    model.addAttribute("cus", new Cus());
-    model.addAttribute("cusDetails", new CusDetails());
   }
 
   @PostMapping("/register")
