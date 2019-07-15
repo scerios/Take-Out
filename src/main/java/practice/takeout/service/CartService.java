cb48a28a93e1144141007af10bd814801ad82543
Takács Róbert <89.t.robert@gmail.com>
2019-07-15 16:35:38 +0200

    feat(CartService): added setBurgerAdded()

--- src/main/java/practice/takeout/service/CartService.java
+++ src/main/java/practice/takeout/service/CartService.java
@@ -1,7 +1,11 @@
 package practice.takeout.service;
 
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import practice.takeout.model.Cart;
+import practice.takeout.model.PopUpMsq;
 
 public interface CartService {
   void addBurgerToCart(Cart cart, long burgerId, int quantity);
+
+  void setBurgerAdded(PopUpMsq popUpMsq, RedirectAttributes redirectAttributes);
 }
--- src/main/java/practice/takeout/service/CartServiceImpl.java
+++ src/main/java/practice/takeout/service/CartServiceImpl.java
@@ -1,7 +1,9 @@
 package practice.takeout.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import practice.takeout.model.Cart;
+import practice.takeout.model.PopUpMsq;
 import practice.takeout.repository.CartRepository;
 
 @Service
@@ -17,5 +19,11 @@ public class CartServiceImpl implements CartService {
     cart.setBurgerId(burgerId);
     cart.setQuantity(quantity);
     repository.save(cart);
+  }
+
+  @Override
+  public void setBurgerAdded(PopUpMsq popUpMsq, RedirectAttributes redirectAttributes) {
+    popUpMsq.setAddedToCart("addedToCart");
+    redirectAttributes.addFlashAttribute("addedToCart", popUpMsq.getAddedToCart());
   }
 }
