bef1cbf429536c0eac34a3b900b81e395ff873fe
Takács Róbert <89.t.robert@gmail.com>
2019-07-15 16:36:50 +0200

    Cart is tested, working as intended

--- src/main/java/practice/takeout/controller/OrderController.java
+++ src/main/java/practice/takeout/controller/OrderController.java
@@ -3,12 +3,11 @@ package practice.takeout.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import practice.takeout.model.Burger;
-import practice.takeout.model.OrderDetails;
-import practice.takeout.service.BurgerServiceImpl;
-import practice.takeout.service.CusServiceImpl;
-import practice.takeout.service.OrderDetailsServiceImpl;
-import practice.takeout.service.OrderServiceImpl;
+import practice.takeout.model.Cart;
+import practice.takeout.model.PopUpMsq;
+import practice.takeout.service.*;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -17,25 +16,29 @@ public class OrderController {
   private OrderDetailsServiceImpl orderDetailsService;
   private CusServiceImpl cusService;
   private BurgerServiceImpl burgerService;
+  private CartServiceImpl cartService;
 
   @Autowired
   public OrderController(OrderServiceImpl orderService, OrderDetailsServiceImpl orderDetailsService,
-                         CusServiceImpl cusService, BurgerServiceImpl burgerService) {
+                         CusServiceImpl cusService, BurgerServiceImpl burgerService, CartServiceImpl cartService) {
     this.orderService = orderService;
     this.orderDetailsService = orderDetailsService;
     this.cusService = cusService;
     this.burgerService = burgerService;
+    this.cartService = cartService;
   }
 
   @PostMapping("/order")
-  public String addBurgerToCart(Burger burger, OrderDetails orderDetails, HttpSession session) {
+  public String addBurgerToCart(Burger burger, Cart cart, HttpSession session, PopUpMsq popUpMsq,
+                                RedirectAttributes redirectAttributes) {
     long burgerId = burgerService.getBurgerIdByQuery(burger.getBun(), burger.getMeat(), burger.getCheese(), burger.getSauce());
     if (burgerId == 0) {
       burgerService.addBurger(burger);
       burgerId = burgerService.getBurgerIdByQuery(burger.getBun(), burger.getMeat(), burger.getCheese(), burger.getSauce());
     }
-    orderDetailsService.addOrderDetails(orderDetails, burgerId);
-    orderService.addOrder(cusService.getCusSessionId(session));
+    cusService.addCartToCus(cusService.getCusSessionId(session), cart);
+    cartService.addBurgerToCart(cart, burgerId, cart.getQuantity());
+    cartService.setBurgerAdded(popUpMsq, redirectAttributes);
     return "redirect:/order";
   }
 }
