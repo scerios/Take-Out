8d66f43cab65492aa49ec9ccd03f713725edb941
Takács Róbert <89.t.robert@gmail.com>
2019-07-15 16:09:01 +0200

    feat(Cart): added Cart to store items temporarily before order

---
+++ src/main/java/practice/takeout/model/Cart.java
@@ -0,0 +1,52 @@
+package practice.takeout.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Cart {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+  private long burgerId;
+  private int quantity;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "cus_id", nullable = false)
+  private Cus cus;
+
+  public Cart() {
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public long getBurgerId() {
+    return burgerId;
+  }
+
+  public void setBurgerId(long burgerId) {
+    this.burgerId = burgerId;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  public Cus getCus() {
+    return cus;
+  }
+
+  public void setCus(Cus cus) {
+    this.cus = cus;
+  }
+}
