af14e013aaeec46de862e8c035fc3c29b365c738
Takács Róbert <89.t.robert@gmail.com>
2019-07-18 15:01:50 +0200

    fix(OrderDetailsService): addOrderDetails() redesigned

--- src/main/java/practice/takeout/service/OrderDetailsService.java
+++ src/main/java/practice/takeout/service/OrderDetailsService.java
@@ -1,7 +1,10 @@
 package practice.takeout.service;
 
+import practice.takeout.dto.BurgerQuantityDto;
 import practice.takeout.model.Order;
 
+import java.util.List;
+
 public interface OrderDetailsService {
-  void addOrderDetails(Order order, long burgerId, int quantity);
+  void addOrderDetails(Order order, List<BurgerQuantityDto> burgerQuantityDtoList);
 }
--- src/main/java/practice/takeout/service/OrderDetailsServiceImpl.java
+++ src/main/java/practice/takeout/service/OrderDetailsServiceImpl.java
@@ -1,9 +1,12 @@
 package practice.takeout.service;
 
 import org.springframework.stereotype.Service;
+import practice.takeout.dto.BurgerQuantityDto;
 import practice.takeout.model.Order;
 import practice.takeout.model.OrderDetails;
 import practice.takeout.repository.OrderDetailsRepository;
+
+import java.util.List;
 
 @Service
 public class OrderDetailsServiceImpl implements OrderDetailsService {
@@ -14,11 +17,13 @@ public class OrderDetailsServiceImpl implements OrderDetailsService {
   }
 
   @Override
-  public void addOrderDetails(Order order, long burgerId, int quantity) {
-    OrderDetails orderDetails = new OrderDetails();
-    orderDetails.setOrder(order);
-    orderDetails.setBurgerId(burgerId);
-    orderDetails.setQuantity(quantity);
-    repository.save(orderDetails);
+  public void addOrderDetails(Order order, List<BurgerQuantityDto> burgerQuantityDtoList) {
+    for (int i = 0; i < burgerQuantityDtoList.size(); i++) {
+      OrderDetails orderDetails = new OrderDetails();
+      orderDetails.setOrder(order);
+      orderDetails.setBurgerId(burgerQuantityDtoList.get(i).getBurgerId());
+      orderDetails.setQuantity(burgerQuantityDtoList.get(i).getQuantity());
+      repository.save(orderDetails);
+    }
   }
 }
