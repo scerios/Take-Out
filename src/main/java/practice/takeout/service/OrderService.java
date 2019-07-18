7ad53385072d309f3052cc2e5ce681b47350e120
Takács Róbert <89.t.robert@gmail.com>
2019-07-18 15:32:29 +0200

    feat(OrderService): added getLastOrderByCusDetailsId()

--- src/main/java/practice/takeout/service/OrderService.java
+++ src/main/java/practice/takeout/service/OrderService.java
@@ -8,4 +8,6 @@ public interface OrderService {
   Order addOrder(CusDetails cusDetails);
 
   void addDetailsToOrder(long id, OrderDetails orderDetails);
+
+  Order getLastOrderByCusDetailsId(long id);
 }
--- src/main/java/practice/takeout/service/OrderServiceImpl.java
+++ src/main/java/practice/takeout/service/OrderServiceImpl.java
@@ -1,14 +1,21 @@
 package practice.takeout.service;
 
 import org.springframework.stereotype.Service;
+import practice.takeout.dataProvider.SystemDefaults;
 import practice.takeout.model.CusDetails;
 import practice.takeout.model.Order;
 import practice.takeout.model.OrderDetails;
 import practice.takeout.repository.OrderRepository;
+import java.sql.*;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+  private SystemDefaults defaults = new SystemDefaults();
   private OrderRepository repository;
+  private final String JDBC_DRIVER = defaults.getJdbcDriver();
+  private final String DB_URL = defaults.getDbUrl();
+  private final String USERNAME = defaults.getUserName();
+  private final String PWD = defaults.getPwd();
 
   public OrderServiceImpl(OrderRepository repository) {
     this.repository = repository;
@@ -26,5 +33,32 @@ public class OrderServiceImpl implements OrderService {
   @Override
   public void addDetailsToOrder(long id, OrderDetails orderDetails) {
     repository.findById(id).get().addOrderDetails(orderDetails);
+  }
+
+  @Override
+  public Order getLastOrderByCusDetailsId(long id) {
+    final String query = "SELECT order_id FROM orders WHERE customer_details_id = " + "\"" + id + "\""
+        + "ORDER BY order_id DESC LIMIT 1";
+    Order order = new Order();
+    PreparedStatement ps;
+    Connection conn;
+    try {
+      Class.forName(JDBC_DRIVER);
+      conn = DriverManager.getConnection(DB_URL, USERNAME, PWD);
+      ps = conn.prepareStatement(query);
+      ResultSet rs = ps.executeQuery(query);
+      while (rs.next()) {
+        order = repository.findById(rs.getLong("order_id")).get();
+      }
+      ps.close();
+      conn.close();
+    } catch (ClassNotFoundException CNFe) {
+      throw new RuntimeException("JDBC Driver cannot be found.");
+    } catch (SQLException SQLe) {
+      throw new RuntimeException("SQL Database cannot be found.");
+    } catch (Exception e) {
+      throw new RuntimeException("Cannot identify the problem.");
+    }
+    return order;
   }
 }
